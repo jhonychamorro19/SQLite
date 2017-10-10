f94a346c743b00e7993a0a9b3986c5ee8c120daa
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-10-09 21:44:31 -0500

    agregando datos a tablas

--- SQLite/SQLite/HomePage.xaml.cs
+++ SQLite/SQLite/HomePage.xaml.cs
@@ -15,6 +15,60 @@ namespace SQLite
         public HomePage()
         {
             InitializeComponent();
+
+            using (var datos = new DataAccess())
+            {                
+                listaListView.ItemsSource = datos.GetEmpleados();
+            }
+
+            agregarButton.Clicked += AgregarButton_Clicked;
+
+        }
+
+        private async void AgregarButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nombresEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar nombres", "Aceptar");
+                nombresEntry.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(apellidosEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar apellidos", "Aceptar");
+                apellidosEntry.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(salarioEntry.Text))
+            {
+                await DisplayAlert("Error", "Debe ingresar salario", "Aceptar");
+                salarioEntry.Focus();
+                return;
+            }
+
+            var empleado = new Empleado
+            {
+                Nombres = nombresEntry.Text,
+                Apellidos = apellidosEntry.Text,
+                FechaContrato = fechaContratoDatePicker.Date,
+                Salario = Decimal.Parse(salarioEntry.Text),
+                Activo = activoSwitch.IsToggled
+            };
+
+            using (var datos = new DataAccess())
+            {
+                datos.InsertEmpleado(empleado);
+                listaListView.ItemsSource = datos.GetEmpleados();
+            }
+
+            nombresEntry.Text = string.Empty;
+            apellidosEntry.Text = string.Empty;
+            salarioEntry.Text = string.Empty;
+            fechaContratoDatePicker.Date = DateTime.Now;
+            activoSwitch.IsToggled = true;
+            await DisplayAlert("Confirmacion", "Empleado creado", "Aceptar");
         }
     }
 }
